dea181786d3fdfe4ddd14a005b06ca97312b15b4
lamhoangtung <lamhoangtung.vz@gmail.com>
2018-11-22 22:43:20 +0700

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -1,73 +1,58 @@
 import ast
-import datetime as dt
 import os
 
 import cv2
-import keras
-import matplotlib.pyplot as plt
 import numpy as np
+from tensorflow.keras.applications.densenet import preprocess_input
+
+import keras
 import pandas as pd
-import seaborn as sns
-from tensorflow.keras.applications.inception_v3 import preprocess_input
 
-from main import batchsize, size
-
-plt.rcParams['figure.figsize'] = [16, 10]
-plt.rcParams['font.size'] = 14
-
-DP_DIR = '/mnt/raid1/kaggle/shuffle-csvs/'
-INPUT_DIR = '/mnt/raid1/kaggle/'
-
-BASE_SIZE = 256
-NCSVS = 100
-NCATS = 340
-np.random.seed(seed=1987)
+INPUT_SHUFFLED_DIR = '/mnt/raid1/kaggle/shuffle-csvs/'
+NUMBERS_OF_CLASSES = 340
+IMAGE_SIZE = 128
+VALIDATION_SIZE = 5000
 
 
-def f2cat(filename: str) -> str:
-    return filename.split('.')[0]
-
-
-def list_all_categories():
-    files = os.listdir(os.path.join(INPUT_DIR, 'train_simplified'))
-    return sorted([f2cat(f) for f in files], key=str.lower)
-
-
-def draw_cv2(raw_strokes, size=256, lw=6, time_color=True):
-    img = np.zeros((BASE_SIZE, BASE_SIZE), np.uint8)
+def draw_image(raw_strokes, size=IMAGE_SIZE, line_width=6, time_color=True):
+    img = np.zeros((size, size), np.uint8)
     for t, stroke in enumerate(raw_strokes):
-        for i in range(len(stroke[0]) - 1):
+        for i in range(len(stroke[0])-1):
             color = 255 - min(t, 10) * 13 if time_color else 255
             _ = cv2.line(img, (stroke[0][i], stroke[1][i]),
-                         (stroke[0][i + 1], stroke[1][i + 1]), color, lw)
-    if size != BASE_SIZE:
-        return cv2.resize(img, (size, size))
-    else:
-        return img
+                         (stroke[0][i + 1], stroke[1][i + 1]), color, line_width)
+    return img
 
 
-def image_generator_xd(size, batchsize, ks, lw=6, time_color=True):
+def image_generator(batch_size, ks):
     while True:
         for k in np.random.permutation(ks):
-            filename = os.path.join(DP_DIR, 'train_k{}.csv.gz'.format(k))
-            for df in pd.read_csv(filename, chunksize=batchsize):
+            filename = os.path.join(
+                INPUT_SHUFFLED_DIR, 'train_k{}.csv.gz'.format(k))
+            for df in pd.read_csv(filename, chunksize=batch_size):
                 df['drawing'] = df['drawing'].apply(ast.literal_eval)
-                x = np.zeros((len(df), size, size, 1))
+                x = np.zeros((len(df), IMAGE_SIZE, IMAGE_SIZE, 1))
                 for i, raw_strokes in enumerate(df.drawing.values):
-                    x[i, :, :, 0] = draw_cv2(raw_strokes, size=size, lw=lw,
-                                             time_color=time_color)
+                    x[i, :, :, 0] = draw_image(raw_strokes)
                 x = preprocess_input(x).astype(np.float32)
-                y = keras.utils.to_categorical(df.y, num_classes=NCATS)
+                y = keras.utils.to_categorical(
+                    df.y, num_classes=NUMBERS_OF_CLASSES)
                 yield x, y
 
 
-def df_to_image_array_xd(df, size, lw=6, time_color=True):
+def df_to_image_array(df):
     df['drawing'] = df['drawing'].apply(ast.literal_eval)
-    x = np.zeros((len(df), size, size, 1))
+    x = np.zeros((len(df), IMAGE_SIZE, IMAGE_SIZE, 1))
     for i, raw_strokes in enumerate(df.drawing.values):
-        x[i, :, :, 0] = draw_cv2(
-            raw_strokes, size=size, lw=lw, time_color=time_color)
+        x[i, :, :, 0] = draw_image(raw_strokes)
     x = preprocess_input(x).astype(np.float32)
     return x
 
 
+valid_df = pd.read_csv(os.path.join(
+    INPUT_SHUFFLED_DIR, 'train_k{}.csv.gz'.format(99)), nrows=VALIDATION_SIZE)
+x_valid = df_to_image_array(valid_df)
+y_valid = keras.utils.to_categorical(
+    valid_df.y, num_classes=NUMBERS_OF_CLASSES)
+print(x_valid.shape, y_valid.shape)
+print('Validation array memory {:.2f} GB'.format(x_valid.nbytes / 1024.**3))
