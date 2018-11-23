c679522187dde4e0bb39484ab1f11e2c4ea907fa
lamhoangtung <lamhoangtung.vz@gmail.com>
2018-11-23 22:41:26 +0700

    Add Callback: Tensorboard and Save Checkpoint

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
+import ast
+import datetime as dt
+import os
+import time
+from math import trunc
+
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.applications.inception_v3 import InceptionV3
-from tensorflow.keras.metrics import categorical_accuracy, top_k_categorical_accuracy, categorical_crossentropy
-from tensorflow.keras.optimizers import Adam
-from data_generator import *
+
+import pandas as pd
+import seaborn as sns
+from keras.applications.densenet import DenseNet121, preprocess_input
+from keras.callbacks import ReduceLROnPlateau
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.metrics import (categorical_accuracy, categorical_crossentropy,
+                           top_k_categorical_accuracy)
+from keras.models import Model
+from keras.optimizers import Adam, ModelCheckpoint
+from keras.callbacks import TensorBoard
+
+start = dt.datetime.now()
+
+
+DP_DIR = '/mnt/raid1/kaggle/shuffle-csvs/'
+INPUT_DIR = '/mnt/raid1/kaggle/'
+
+BASE_SIZE = 256
+NCSVS = 100
+NCATS = 340
+np.random.seed(seed=2018)
+tf.set_random_seed(seed=2018)
+
+
+def f2cat(filename: str) -> str:
+    return filename.split('.')[0]
+
+
+def list_all_categories():
+    files = os.listdir(os.path.join(INPUT_DIR, 'train_simplified'))
+    return sorted([f2cat(f) for f in files], key=str.lower)
+
 
 def apk(actual, predicted, k=3):
     """
@@ -40,21 +75,65 @@ def top_3_accuracy(y_true, y_pred):
     return top_k_categorical_accuracy(y_true, y_pred, k=3)
 
 
-STEPS = 800
-EPOCHS = 16
-size = 139
-batchsize = 680
-NCATS = 340
+batchsize = 330
+size = 64
+STEPS = 10000
+EPOCHS = trunc(30060000/(batchsize*STEPS))
+# STEPS = trunc((34000000/EPOCHS)/batchsize)
 
-model = InceptionV3(include_top=True, weights=None,
-                    input_shape=(size, size, 1), classes=NCATS)
-model.compile(optimizer=Adam(lr=0.002), loss='categorical_crossentropy', metrics=[
+base_model = DenseNet121(include_top=False, weights='imagenet',
+                         input_shape=(size, size, 3), classes=NCATS)
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(NCATS, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+model.compile(optimizer=Adam(lr=1e-4, decay=1e-9), loss='categorical_crossentropy', metrics=[
               categorical_crossentropy, categorical_accuracy, top_3_accuracy])
-
 print(model.summary())
 
+
+def draw_cv2(raw_strokes, size=256, lw=6, time_color=True):
+    img = np.zeros((BASE_SIZE, BASE_SIZE), np.uint8)
+    for t, stroke in enumerate(raw_strokes):
+        for i in range(len(stroke[0]) - 1):
+            color = 255 - min(t, 10) * 13 if time_color else 255
+            _ = cv2.line(img, (stroke[0][i], stroke[1][i]),
+                         (stroke[0][i + 1], stroke[1][i + 1]), color, lw)
+    img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    if size != BASE_SIZE:
+        return cv2.resize(img, (size, size))
+    else:
+        return img
+
+
+def image_generator_xd(size, batchsize, ks, lw=6, time_color=True):
+    while True:
+        for k in np.random.permutation(ks):
+            filename = os.path.join(DP_DIR, 'train_k{}.csv.gz'.format(k))
+            for df in pd.read_csv(filename, chunksize=batchsize):
+                df['drawing'] = df['drawing'].apply(ast.literal_eval)
+                x = np.zeros((len(df), size, size, 3))
+                for i, raw_strokes in enumerate(df.drawing.values):
+                    x[i, :, :, :] = draw_cv2(raw_strokes, size=size, lw=lw,
+                                             time_color=time_color)
+                x = preprocess_input(x).astype(np.float32)
+                y = keras.utils.to_categorical(df.y, num_classes=NCATS)
+                yield x, y
+
+
+def df_to_image_array_xd(df, size, lw=6, time_color=True):
+    df['drawing'] = df['drawing'].apply(ast.literal_eval)
+    x = np.zeros((len(df), size, size, 3))
+    for i, raw_strokes in enumerate(df.drawing.values):
+        x[i, :, :, :] = draw_cv2(
+            raw_strokes, size=size, lw=lw, time_color=time_color)
+    x = preprocess_input(x).astype(np.float32)
+    return x
+
+
 valid_df = pd.read_csv(os.path.join(
-    DP_DIR, 'train_k{}.csv.gz'.format(NCSVS - 1)), nrows=34000)
+    DP_DIR, 'train_k{}.csv.gz'.format(NCSVS - 1)))
 x_valid = df_to_image_array_xd(valid_df, size)
 y_valid = keras.utils.to_categorical(valid_df.y, num_classes=NCATS)
 print(x_valid.shape, y_valid.shape)
@@ -75,41 +154,44 @@ for i in range(n**2):
 plt.tight_layout()
 fig.savefig('gs.png', dpi=300)
 
-callbacks = [
-    ReduceLROnPlateau(monitor='val_categorical_accuracy', factor=0.5, patience=5,
-                      min_delta=0.005, mode='max', cooldown=3, verbose=1)
-]
-hists = []
+tensorboard = TensorBoard(log_dir="logs/{}".format(time()), histogram_freq=1, batch_size=batchsize, write_grads=True, write_images=True, update_feq=(STEPS/10)*batchsize)
+weightpath = "weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+checkpoint = ModelCheckpoint(weightpath, monitor='val_loss', verbose=0,save_best_only=False, save_weights_only=False, mode='auto', period=1)
+
+
 hist = model.fit_generator(
     train_datagen, steps_per_epoch=STEPS, epochs=EPOCHS, verbose=1,
     validation_data=(x_valid, y_valid),
-    callbacks=callbacks
+    callback=[tensorboard, checkpoint]
 )
-hists.append(hist)
+model.save('model.h5')
 
-hist_df = pd.concat([pd.DataFrame(hist.history) for hist in hists], sort=True)
-hist_df.index = np.arange(1, len(hist_df)+1)
-fig, axs = plt.subplots(nrows=2, sharex=True, figsize=(16, 10))
-axs[0].plot(hist_df.val_categorical_accuracy,
-            lw=5, label='Validation Accuracy')
-axs[0].plot(hist_df.categorical_accuracy, lw=5, label='Training Accuracy')
-axs[0].set_ylabel('Accuracy')
-axs[0].set_xlabel('Epoch')
-axs[0].grid()
-axs[0].legend(loc=0)
-axs[1].plot(hist_df.val_categorical_crossentropy,
-            lw=5, label='Validation MLogLoss')
-axs[1].plot(hist_df.categorical_crossentropy, lw=5, label='Training MLogLoss')
-axs[1].set_ylabel('MLogLoss')
-axs[1].set_xlabel('Epoch')
-axs[1].grid()
-axs[1].legend(loc=0)
-fig.savefig('hist.png', dpi=300)
 
 valid_predictions = model.predict(x_valid, batch_size=128, verbose=1)
 map3 = mapk(valid_df[['y']].values, preds2catids(valid_predictions).values)
 print('Map3: {:.3f}'.format(map3))
 
 
+test = pd.read_csv(os.path.join(INPUT_DIR, 'test_simplified.csv'))
+test.head()
+x_test = df_to_image_array_xd(test, size)
+print(test.shape, x_test.shape)
+print('Test array memory {:.2f} GB'.format(x_test.nbytes / 1024.**3))
 
 
+test_predictions = model.predict(x_test, batch_size=128, verbose=1)
+
+top3 = preds2catids(test_predictions)
+
+
+cats = list_all_categories()
+id2cat = {k: cat.replace(' ', '_') for k, cat in enumerate(cats)}
+top3cats = top3.replace(id2cat)
+
+test['word'] = top3cats['a'] + ' ' + top3cats['b'] + ' ' + top3cats['c']
+submission = test[['key_id', 'word']]
+submission.to_csv('gs_mn_submission_{}.csv'.format(
+    int(map3 * 10**4)), index=False)
+
+end = dt.datetime.now()
+print('Latest run {}.\nTotal time {}s'.format(end, (end - start).seconds))
